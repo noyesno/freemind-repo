ca4274078f886ab6a2beca227a3fc3b1754b0d0b
noyesno <noyesno.net@gmail.com>
2018-05-13 20:18:20 +0800

    init freemind mindmap file filter bin/mmclean.tcl

---
+++ bin/mmclean.tcl
@@ -0,0 +1,42 @@
+#!/usr/bin/env tclsh
+
+
+proc decode_entities {text} {
+  set matches [regexp -all -inline -indices {&#x[a-fA-F0-9]+;} $text]
+  if {[llength $matches]>0} {
+    set head 0
+    set buf ""
+    foreach pair $matches {
+      lassign $pair a b
+      set hex [string range $text $a+3 $b-1]
+      append buf [string range $text $head $a-1]
+      append buf [format "%c" "0x$hex"]
+      set head [expr {$b+1}]
+    }
+    append buf [string range $text $b+1 end]
+    set text $buf
+  }
+  return $text
+}
+
+proc decode_entities {text} {
+  set text [subst -nocommand -novariable [regsub -all {&#x([a-fA-F0-9]+);} [string map {"\\" "\\\\"} $text] {\u\1}]]
+
+  return $text
+}
+
+proc remove_freemind_dummy {text} {
+  # set text [regsub -all {ID="\w+"|CREATED="\d+"|MODIFIED="\d+"|FOLDED="(true|false)"} $text ""]
+  set text [regsub -all {ID="\w+"|CREATED="\d+"|MODIFIED="\d+"} $text ""]
+  #set text [regsub -all {CREATED="\d+"|MODIFIED="\d+"} $text ""]
+  set text [regsub -all {<node\s{2,}} $text "<node "]
+  return $text
+}
+
+
+puts {<?xml version="1.0" encoding="utf-8"?>}
+while {[gets stdin line]>=0} {
+  set line [decode_entities $line]
+  set line [remove_freemind_dummy $line]
+  puts $line
+}
